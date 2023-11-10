58849347328ffd3e546f4f8bd9ac7e8665061b7e
Olley <goodnewsolley@gmail.com>
2023-11-10 15:56:53 +1100

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Olley!'));
+app.get('/', (req, res) => res.send('Hello again, Olley!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
